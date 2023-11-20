760a7ef89dff2dddc7074992fe9efcadb406acc4
ohtmddud <ohsy94@gmail.com>
2023-11-20 12:36:40 +0900

    dd

--- app/src/androidTest/java/MyComposeTest.kt
+++ app/src/androidTest/java/MyComposeTest.kt
@@ -1,2 +1,52 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.google.samples.apps.nowinandroid.MainActivity
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+@HiltAndroidTest
 class MyComposeTest {
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @BindValue
+    @get:Rule
+    val tmpFolder: TemporaryFolder = TemporaryFolder.builder().assureDeletion().build()
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun MyTest() {
+        composeTestRule
+            .onNodeWithText("이 글이 화면에 보이는지 테스트해주세요")
+            .assertIsDisplayed()
+    }
+    @Test
+    fun YourTest() {
+        composeTestRule
+            .onNodeWithText("다른 글을 테스트해보겠습니다")
+            .assertIsDisplayed()
+    }
 }
